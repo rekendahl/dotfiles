24e9ac29ff8f0162905baf890556c24b96b33b80
rekendahl <rekendahl@testevolution.com>
2012-12-18 10:23:59 -0500

    Fixed script

--- link_to_files.sh
+++ link_to_files.sh
@@ -19,20 +19,19 @@ mv .gvimrc .gvimrc_pre_dotfiles
 ln -s $DOT_FILES/gvimrc .gvimrc
 
 cd $DOT_FILES
-git submoudle init
-git submoudle update
+git submodule init
+git submodule update
 cd -
 
 # Make sure we have patched fonts to make it look pretty. 
 # FIXME: For some reason it didn't work with symbolic links on the mac so I use cp -f instead.
-$font_dir = ""
 if [ "$OSTYPE" = "linux-gnu" ];
 then
    echo "Copying Patched Powerlne Source Code Pro fonts to ~/.fonts"
-   $font_dir = "~/fonts/."
+   export font_dir=~/.fonts/.
 else
    echo "Copying Patched Powerlne Source Code Pro fonts to ~/Librarys/Fonts"
-   $font_dir = "~/Library/Fonts/."
+   export font_dir=~/Library/Fonts/.
 fi
 cp -f Projects/dotfiles/patched_powerline_fonts/vimstuff/powerline-fonts/SourceCodePro-*.otf $font_dir
 
